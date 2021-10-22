ba592fc79819022535963d727fb564cf22c65df8
zoranzoki21 <zorandori4444@gmail.com>
2021-10-22 23:22:50 +0200

    Add grunt-banana-checker, grunt-eslint and grunt-stylelint
    grunt-banana-checker will test i18n files
    grunt-eslint will test .js and .json files
    grunt-stylelint will test .css and .less files
    
    Change-Id: I9a66ffe2f03e569808d7cd5fea11237d0b92a941

--- modules/stickytoc.js
+++ modules/stickytoc.js
@@ -1,4 +1,5 @@
 $( function () {
+	// eslint-disable-next-line no-jquery/no-global-selector
 	var $toc = $( '#toc' ),
 		float = mw.config.get( 'wgStickyTOCFloat' ),
 		forceSticky = mw.config.get( 'wgStickyTOCNavbar' ),
@@ -9,23 +10,24 @@ $( function () {
 
 	if ( $toc.length ) {
 
-		if( replaceTitle && articleTitle ) {
-			$toc.find('#mw-toc-heading').text( articleTitle );
+		if ( replaceTitle && articleTitle ) {
+			$toc.find( '#mw-toc-heading' ).text( articleTitle );
 		}
 
 		$toc.insertBefore( '#content' );
+		/* eslint-disable mediawiki/class-doc, no-jquery/no-parse-html-literal */
 		$wrapper = $( '<div/>' )
+		/* eslint-enable */
 			.addClass( 'sticky-top' )
 			.addClass( 'stickytoc' )
 			.addClass( 'stickytoc-pull-' + float );
-		if( float === 'aside' ) {
+		if ( float === 'aside' ) {
 			$parent = $toc.parent();
 			$parent.addClass( 'stickytoc-parent' );
 		}
-		if( forceSticky ) {
+		if ( forceSticky ) {
 			$wrapper.addClass( 'force-sticky' );
 		}
 		$toc.wrap( $wrapper );
 	}
 } );
-
